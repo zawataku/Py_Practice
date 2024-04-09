1d1aceb46eff921147e6b6f889c13006bca5cd11
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-04-09 16:17:14 +0900

    FizzBuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,11 @@
+x = int(input())
+
+for i in range(1, x+1):
+    if (i % 3 == 0 and i % 5 == 0):
+        print("Fizz Buzz")
+    elif (i % 5 == 0):
+        print("Buzz")
+    elif (i % 3 == 0):
+        print("Fizz")
+    else:
+        print(i)
